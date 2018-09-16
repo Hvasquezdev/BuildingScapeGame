e26eab793fb1c39cd8481a37254c3fadca11e19e
Hector Vasquez <hvasquezdev@gmail.com>
2018-09-16 00:15:09 -0400

    Macros Starting with UPROPERTY

--- Source/BuildingScape/OpenDoor.h
+++ Source/BuildingScape/OpenDoor.h
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -24,6 +24,10 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 70.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume *PressurePlate;
 };
